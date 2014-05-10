8ca9e1676a989e14d2fccc6547e2be6a05bd90c1
Aashish Kapur <aashish97+github@gmail.com>
2014-05-10 00:04:39 -0700

    map.php

---
+++ map.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+
+<iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBEPTzQE5w10jXl2qe7yEKuzpaXm8TeTHM&q=Google">
+</iframe>
+
+</body>
+
+</html>
